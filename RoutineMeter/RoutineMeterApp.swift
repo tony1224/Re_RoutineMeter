6afa41a6f5b61b5187662606b412866bb1bd46ab
admin <admin@adminnoMacBook-Pro.local>
2024-04-23 09:12:59 +0900

    first commit

---
+++ RoutineMeter/RoutineMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineMeterApp.swift
+//  RoutineMeter
+//
+//  Created by admin on 2024/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RoutineMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
